f6798862025a99116e518f09920c10746355d011
Yusa Islam <yusaislam48@gmail.com>
2021-02-07 23:34:51 +0600

    get artist name

--- app.js
+++ app.js
@@ -11,18 +11,25 @@ const displaySong = songs => {
     const songContainer = document.getElementById('songContainer');
 
     songs.forEach(song => {
-        // console.log(song.artist);
+        // console.log(song);
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
         songDiv.innerHTML = `
             <div class="col-md-9">
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                <audio controls>
+                    <source src="${song.preview}" type="audio/mpeg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
         `;
         songContainer.appendChild(songDiv);
     });
+}
+
+const getLyric = (artist, title) =>{
+    console.log(artist, title);
 }
